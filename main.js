e915fa2a85302e490c7b8aeb43614cb676396ddd
Adam Vekris <avek4452@stu.raritanval.edu>
2023-04-29 12:49:58 -0400

    Error encountered

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 /* fetch.js */
+// fetch('demo.json').then(res => {
+//     console.log(res);
+//     return res.json();
+//     })
+//      .then(json => console.log(json));
+
+fetch('https://unsplash.it/600/400')
+.then(res => res.blob())
+.then(blob => {
+    let img = document.createElement('img');
+    img.src = URL.createObjectURL(blob);
+    document.querySelector('body').appendChild(img);
+});
